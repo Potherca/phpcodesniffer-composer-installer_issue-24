ffd3565edade71e9716f019dfc34a86eca57c81a
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-05-03 14:48:08 +0200

    Changes to improve the AbstractInstallerTest class
    - Changes delete logic to be more precise in what it deletes.
    - Makes various test methods and assert re-usable.
    - Reduces the amount of static methods.

--- tests/AbstractInstallerTest.php
+++ tests/AbstractInstallerTest.php
@@ -4,54 +4,97 @@ namespace Dealerdirect\Composer\Plugin\Installers\PHPCodeSniffer;
 
 require_once __DIR__ . '/AbstractTestCase.php';
 
+/** @noinspection PhpUndefinedClassInspection */
 abstract class AbstractInstallerTest  extends AbstractTestCase
 {
+    ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
     /** @var array */
-    private static $output = [];
+    private static $packages = [
+        'dealerdirect/phpcodesniffer-composer-installer',
+        'drupal/coder',
+        'frenck/php-compatibility',
+    ];
+
+    //////////////////////////// SETTERS AND GETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
+    private function getVendorDirectory()
+    {
+        return static::getComposerDirectory() . '/vendor';
+    }
+
+    /**
+     * @return string
+     */
+    final public function getCodesnifferConfigurationPath()
+    {
+        return $this->getVendorDirectory() . '/squizlabs/php_codesniffer/CodeSniffer.conf';
+    }
+
+    //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    final public static function setUpBeforeClass()
+    {
+    }
+
+    final public function tearDown()
+    {
+        $this->removeFile(static::getComposerDirectory().'/composer.lock', 'composer lock file');
+    }
 
     /**
      * 1. Install the installer and two sniffs as composer dependency.
      */
-    final public static function setUpBeforeClass()
+    final public function testComposerShouldInstallBothSniffsWhenAskedTo()
     {
-        $vendorDirectory = static::getVendorDirectory();
+        $packages = self::$packages;
 
-        // Remove any previously installed vendors
-        if (is_dir($vendorDirectory)) {
-            $removed = self::removeDirectory($vendorDirectory);
+        $this->removePreviousInstall($packages);
 
-            self::assertTrue($removed, 'Could not remove "vendor" directory');
-        }
+        $output = $this->installPackages($packages);
+        $vendorDirectory = $this->getVendorDirectory();
 
-        // Install the installer and sniffs
-        $command = 'php composer.phar require --sort-packages "dealerdirect/phpcodesniffer-composer-installer" "frenck/php-compatibility" "drupal/coder" 2>&1';
-
-        exec($command, self::$output, $exitCode);
-
-        self::assertSame(0, $exitCode, 'The "composer require" command did not run successfully.');
+        return [
+            $output,
+            [
+                $vendorDirectory .'/drupal/coder/coder_sniffer',
+                $vendorDirectory .'/frenck/php-compatibility',
+            ],
+        ];
     }
 
     /**
      * 2. Validate the CLI output of composer
+     *
+     * @depends testComposerShouldInstallBothSniffsWhenAskedTo
+     *
+     * @param array[] $params
+     *
+     * @return array
      */
-    final public function testComposerOutputShouldMentionBothSniffsWhenAskedToInstallSniffs()
+    final public function testComposerOutputShouldMentionOfInstalledSniffsWhenSniffsHaveBeenInstalled(array $params)
     {
-        $actual = array_pop(self::$output);
+        list($output, $installed) = $params;
+
+        $actual = array_pop($output);
 
         $expected = sprintf(
-            'PHP CodeSniffer Config installed_paths set to %1$s/drupal/coder/coder_sniffer,%1$s/frenck/php-compatibility',
-            static::getVendorDirectory()
+            'PHP CodeSniffer Config installed_paths set to %s',
+            implode(',', $installed)
         );
 
-        self::assertSame($expected, $actual, 'Composer output did not contain reference to both sniffs');
+        self::assertSame($expected, $actual, 'Composer output did not contain reference to installed sniff(s)');
+
+        return $installed;
     }
 
     /**
      *  3. Validate the contents of `vendor/squizlabs/php_codesniffer/CodeSniffer.conf`
+     *
+     * @depends testComposerOutputShouldMentionOfInstalledSniffsWhenSniffsHaveBeenInstalled
+     *
+     * @param array $installed
      */
-    final public function testCodeSnifferConfigurationFileShouldMentionBothSniffs()
+    final public function testCodeSnifferConfigurationFileShouldMentionBothSniffs(array $installed)
     {
-        $configurationPath = static::getVendorDirectory() . '/squizlabs/php_codesniffer/CodeSniffer.conf';
+        $configurationPath = $this->getCodesnifferConfigurationPath();
 
         self::assertFileExists($configurationPath, 'CodeSniffer configuration file does not exist');
 
@@ -60,39 +103,117 @@ abstract class AbstractInstallerTest  extends AbstractTestCase
         $expected = sprintf(<<<'TXT'
 <?php
  $phpCodeSnifferConfig = array (
-  'installed_paths' => '%1$s/drupal/coder/coder_sniffer,%1$s/frenck/php-compatibility',
+  'installed_paths' => '%s',
 )
 ?>
 TXT
             ,
-            static::getVendorDirectory()
+            implode(',', $installed)
         );
 
-        self::assertSame($expected, $actual, 'Codesniff configuration file did not contain reference to both sniffs');
+        self::assertSame($expected, $actual, 'Codesniff configuration file did not contain reference to installed sniff(s)');
+    }
+
+    ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    /**
+     * @param string $command
+     *
+     * @return array
+     */
+    private function executeComposerCommand($command)
+    {
+        $command = $this->getComposerCommand() . ' '. $command . ' 2>&1';
+        $output = [];
+
+        exec($command, $output, $exitCode);
+
+        $message = vsprintf(
+            'The command "%s" did not run successfully. %s',
+            [
+                $command,
+                implode("\n", $output),
+            ]
+        );
+
+        self::assertSame(0, $exitCode, $message);
+
+        return $output;
+    }
+
+    /**
+     * @param $packages
+     *
+     * @return array
+     */
+    private function installPackages($packages)
+    {
+        $command = 'require --sort-packages "' . implode('" "', $packages) . '"';
+
+        return $this->executeComposerCommand($command);
     }
 
     /**
      * The code to recursively delete a directory is taken from
      * http://andy-carter.com/blog/recursively-remove-a-directory-in-php
      *
+     * The excellent `glob` solutions has been taken from
+     * http://stackoverflow.com/a/33059445/153049
+     *
      * @param string $path
      * @param bool $success
      *
      * @return bool
      */
-    private static function removeDirectory($path, $success = true)
+    private function removeDirectory($path, $success = true)
     {
-        $files = glob($path . '/*');
+        $files = glob($path . '/{,.}[!.,!..]*',GLOB_MARK|GLOB_BRACE);
 
         foreach ($files as $file) {
             if (is_dir($file)) {
-                $success = self::removeDirectory($file, $success) && $success;
+                $success = $this->removeDirectory($file, $success) && $success;
             } else {
                 $success = unlink($file) && $success;
             }
         }
 
+        $success = rmdir($path) && $success;
+
         return $success;
+    }
+
+    /**
+     * @param string $file
+     * @param string $fileType
+     */
+    private function removeFile($file, $fileType = 'file')
+    {
+        if (is_file($file)) {
+            $errorMessage = 'Could not remove '.$fileType;
+
+            $removed = unlink($file);
+
+            self::assertTrue($removed, $errorMessage);
+        }
+    }
+
+    /**
+     * @param array $packages
+     */
+    private function removePreviousInstall(array $packages)
+    {
+        /*/ Remove any previously installed vendors /*/
+        array_walk($packages, function ($package) {
+            $path = static::getVendorDirectory() . '/' . $package;
+            if (is_dir($path)) {
+                $removed = self::removeDirectory($path);
+
+                $message = sprintf('Could not remove "%s" vendor directory', $package);
+                self::assertTrue($removed, $message);
+            }
+        });
+
+        $this->removeFile(static::getComposerDirectory().'/composer.lock', 'composer lock file');
+        $this->removeFile($this->getCodesnifferConfigurationPath(), 'Codesniffer configuration file');
     }
 }
 
